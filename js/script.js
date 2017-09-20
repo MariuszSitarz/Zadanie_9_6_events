56696daf513df18920c2ef77cf38017018b8da44
Mariusz Sitarz <mariusz_sitarz@wp.pl>
2017-09-20 21:31:24 +0200

    added new lines for returning the area of a triangle in js file

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
-
+//alert asking users to type their name
 var name = prompt('Enter your name');
 alert('Hello, ' + name);
 console.log('Hello, ' + name);
 
 
 
+//testing escape quote
+var firstSentence = 'I like a song that is called "Only you".';
+var secondSentence = "I love a song that is called 'Only you'.";
 
+console.log(firstSentence);
+console.log(secondSentence);
+
+// declaring variables and returning the area of a triange
+var a = 12;
+    h = 13;
+triangleArea = (a * h / 2);
+
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
